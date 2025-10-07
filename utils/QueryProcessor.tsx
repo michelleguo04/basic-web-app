63acf8df6f3351a36b44ad7a427758bb898b7728
Michelle Guo <myguo@andrew.cmu.edu>
2025-10-07 10:06:44 -0400

    Updated name Rohan to Michelle Guo

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "Michelle Guo";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
